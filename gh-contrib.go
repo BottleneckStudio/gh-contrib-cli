6e55e4112c458aad2f319991309db4aa0a6412c4
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-11-01 12:20:44 +0800

    Removed entity package and request method
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- gh-contrib.go
+++ gh-contrib.go
@@ -1,27 +1,24 @@
-package main 
+package main
+
 import (
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"github.com/BottleneckStudio/gh-contrib-cli/entity"
 )
-	
-	var (
-		author
-		stringsince 
-		stringuntil 
-		stringrepo 
-		stringapiURL = "https://api.github.com/repos/a-fis"
+
+var (
+	author string
+	since  string
+	until  string
+	repo   string
+	apiURL = "https://api.github.com/repos/a-fis"
 )
 
 // ContributionOptions ...
 type ContributionOptions struct {
-	Author string 
+	Author string
 	Since  string
 	Until  string
 	Repo   string
@@ -37,7 +34,8 @@ func init() {
 func main() {
 	flag.Parse()
 	if contribOptions, err := parseOptions(); err == nil {
-		request(contribOptions)
+		// Do request
+		log.Print(contribOptions)
 	} else {
 		usage()
 	}
@@ -48,9 +46,8 @@ func usage() {
 	fmt.Fprintf(os.Stderr, `gh-contrib version: gh-contrib/1.0.0
 >> HomePage: https://github.com/rbo13/gh-contrib
 >> Issue   : https://github.com/rbo13/gh-contrib/issues
->> Author  : rbo13Usage: gh-contrib-cli -u <username> -sinceOptions:`
-)
-flag.PrintDefaults()
+>> Author  : rbo13Usage: gh-contrib-cli -u <username> -sinceOptions:`)
+	flag.PrintDefaults()
 }
 
 func parseOptions() (*ContributionOptions, error) {
@@ -67,31 +64,33 @@ func parseOptions() (*ContributionOptions, error) {
 	return contributionOptions, nil
 }
 
-func request(contribOptions *ContributionOptions) error {
-	var contributions []entity.Contributions
-	client := http.Client{
-	Timeout: time.Duration(10 * time.Second),
-	}
-	url := fmt.Sprintf("%s/%s/commits?author=%s&since=%s&until=%s&page=1&access_token=11beeff32f34e6798c4cc986d173c8f7189587ad", apiURL, contribOptions.Repo, contribOptions.Author, contribOptions.Since, contribOptions.Until)
-	log.Print(url)
-	resp, err := http.Get(url)
-	log.Print(err)
-	
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	
-	buffer, err := ioutil.ReadAll(resp.Body)
-	
-	log.Print(err)
-	if err != nil {
-		return err
-	}
-	json.Unmarshal(buffer, &contributions)
-	log.Print(len(contributions))
-	for i := 0; i < len(contributions); i++ {
-		log.Print(contributions)
-	}
-	return nil
-}
+// func request(contribOptions *ContributionOptions) error {
+// 	var contributions []entity.Contributions
+
+// 	client := http.Client{
+// 		Timeout: time.Duration(10 * time.Second),
+// 	}
+
+// 	url := fmt.Sprintf("%s/%s/commits?author=%s&since=%s&until=%s&page=1&access_token=11beeff32f34e6798c4cc986d173c8f7189587ad", apiURL, contribOptions.Repo, contribOptions.Author, contribOptions.Since, contribOptions.Until)
+// 	log.Print(url)
+// 	resp, err := http.Get(url)
+// 	log.Print(err)
+
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	buffer, err := ioutil.ReadAll(resp.Body)
+
+// 	log.Print(err)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	json.Unmarshal(buffer, &contributions)
+// 	log.Print(len(contributions))
+// 	for i := 0; i < len(contributions); i++ {
+// 		log.Print(contributions)
+// 	}
+// 	return nil
+// }
